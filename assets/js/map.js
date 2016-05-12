6e0a76ae0f28576a932b5668aba45be3f3ea4f91
spiralray <spiralray@gmail.com>
2016-05-12 12:55:23 +0900

    地点登録可能に

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,24 +1,28 @@
 var stylesArrayMap = [  {    "featureType": "administrative.country",    "elementType": "geometry.stroke",    "stylers": [      { "saturation": -100 },      { "visibility": "off" }    ]  },{    "featureType": "road",    "elementType": "geometry.stroke",    "stylers": [      { "saturation": -100 },      { "weight": 0.5 },      { "visibility": "on" }    ]  },{    "featureType": "landscape",    "stylers": [      { "saturation": -100 },      { "visibility": "simplified" }    ]  },{    "stylers": [      { "saturation": -100 }    ]  },{    "featureType": "landscape.natural",    "elementType": "geometry",    "stylers": [      { "saturation": -100 },      { "lightness": 28 }    ]  },{    "featureType": "road.highway",    "stylers": [      { "saturation": -87 },      { "lightness": 64 }    ]  },{    "featureType": "water",    "stylers": [      { "saturation": -100 },      { "gamma": 0.5 }    ]  },{    "featureType": "poi.park",    "elementType": "geometry",    "stylers": [      { "saturation": -100 },      { "lightness": 82 }    ]  },{    "featureType": "road.arterial",    "elementType": "labels",    "stylers": [      { "visibility": "off" },      { "saturation": 74 }    ]  },{    "featureType": "transit.station.airport",    "elementType": "geometry",    "stylers": [      { "visibility": "on" },      { "saturation": -100 },      { "lightness": -22 },      { "gamma": 0.7 }    ]  },{    "featureType": "landscape",    "stylers": [      { "saturation": -100 }    ]  },{    "featureType": "road",    "elementType": "labels",    "stylers": [      { "lightness": 24 },      { "visibility": "off" },      { "saturation": -100 }    ]  },{    "featureType": "transit.line",    "elementType": "labels.text",    "stylers": [      { "visibility": "off" }    ]  },{    "featureType": "poi",    "elementType": "labels.icon",    "stylers": [      { "saturation": -100 },      { "gamma": 0.81 }    ]  },{    "featureType": "transit.line",    "stylers": [      { "saturation": -100 },      { "lightness": -4 }    ]  },{    "featureType": "administrative.locality",    "elementType": "labels.text",    "stylers": [      { "visibility": "off" }    ]  },{    "featureType": "transit.station",    "elementType": "labels.icon",    "stylers": [      { "visibility": "on" },      { "saturation": -100 },      { "gamma": 0.7 }    ]  },{    "featureType": "administrative.province",    "elementType": "geometry",    "stylers": [      { "visibility": "off" }    ]  },{    "featureType": "administrative.province",    "elementType": "labels.text",    "stylers": [      { "saturation": -100 },      { "gamma": 0.6 }    ]  },{    "featureType": "poi",    "stylers": [      { "saturation": -100 },      { "gamma": 0.96 },      { "visibility": "simplified" }    ]  },{    "featureType": "road.arterial",    "elementType": "labels.icon",    "stylers": [      { "visibility": "off" }    ]  },{    "featureType": "poi",    "elementType": "labels.text",    "stylers": [      { "visibility": "simplified" },      { "lightness": 38 }    ]  }]
 var places = [];
-var new_marker = null;
+var selected_marker = null;
 
 var infowindow = null;
+var place_id = 0;
 
 function initialize() {
 
   $.ajax({
-         type: 'GET',
-         url: 'register_window.txt',
-         dataType: 'text',
-         success: function(data) {
-           infowindow = new google.maps.InfoWindow({
-             content: data
-           });
-         },
-         error:function() {
-             alert('問題がありました。');
-         }
+    type: 'GET',
+    cache : false,
+    url: 'register_window.txt',
+    dataType: 'text',
+    success: function(data) {
+      infowindow = new google.maps.InfoWindow({
+        content: data
+      });
+    },
+    error:function() {
+      alert('問題がありました。');
+    }
   });
+
+  printRegisteredPlaces();
 
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 35.011770, lng: 135.768036},
@@ -56,44 +60,126 @@ function initialize() {
     places.forEach(function(place) {
       /*
       var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-      */
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+    */
 
-      // Create a marker for each place.
-      search_markers.push(new google.maps.Marker({
-        map: map,
-        //icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
+    // Create a marker for each place.
+    search_markers.push(new google.maps.Marker({
+      map: map,
+      //icon: icon,
+      title: place.name,
+      position: place.geometry.location
+    }));
 
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
+    if (place.geometry.viewport) {
+      // Only geocodes have viewport.
+      bounds.union(place.geometry.viewport);
+    } else {
+      bounds.extend(place.geometry.location);
+    }
+  });
+  map.fitBounds(bounds);
+});
+
+// クリックイベントを追加
+map.addListener('dblclick', function(e) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: e.latLng
   });
 
-  // クリックイベントを追加
-  map.addListener('dblclick', function(e) {
-    if( new_marker != null ){
-      new_marker.setMap(null);
+  var place = addPlace("New place", "description", marker.getPosition().lat(), marker.getPosition().lng() );
+
+  marker.addListener('click', function() {
+    selected_marker = marker;
+    var place = getPlaceByPosition( marker.getPosition() );
+    console.log(place);
+    infowindow.open(map, marker);
+    document.getElementById("selected_place_id").value = place["id"];
+    document.getElementById("btn_place_name").value = place["name"];
+    document.getElementById("btn_place_description").value = place["description"];
+  });
+
+  infowindow.setOptions({ maxWidth: 400 });
+
+});
+}
+
+function addPlace(name, description, lat, lng) {
+  places.push( {'id': place_id, 'name': name, 'description': description, 'lat': lat, 'lng': lng} );
+  place_id += 1;
+  printRegisteredPlaces();
+  return places[places.length-1];
+}
+
+function deletePlace(id){
+  for(var i=0; i<places.length; i++){
+    if( places[i].id == id ){
+      places.splice( i, 1 ) ;
     }
-    new_marker = new google.maps.Marker({
-      map: map,
-      position: e.latLng
+  }
+  printRegisteredPlaces();
+}
+
+function getPlaceById(id){
+  for(var i=0; i<places.length; i++){
+    if( places[i].id == id ){
+        return places[i];
+    }
+  }
+  return null;
+}
+
+function getPlaceByPosition(latlng){
+  for(var i=0; i<places.length; i++){
+    if( places[i].lat == latlng.lat() && places[i].lng == latlng.lng() ){
+        return places[i];
+    }
+  }
+  return null;
+}
+
+function deleteThisPlace(){
+  var id = document.getElementById("selected_place_id").value;
+  //var place = getPlaceById(id);
+
+  selected_marker.setMap(null);
+  deletePlace(id);
+}
+
+function placeInfoUpdated(){
+  var id = document.getElementById("selected_place_id").value;
+  var place = getPlaceById(id);
+  place["name"] = document.getElementById("btn_place_name").value;
+  place["description"] = document.getElementById("btn_place_description").value;
+  printRegisteredPlaces();
+}
+
+function printRegisteredPlaces(){
+  var str = "";
+  for(var i=0; i<places.length; i++){
+    str += '<div class="fc-event unselected">' + places[i]["name"] + '<br>\n' + places[i]["description"] + '</div>';
+  }
+  document.getElementById("registered-places").innerHTML = str;
+
+  $('#external-events .unselected').each(function() {
+
+    // store data so the calendar knows to render an event upon drop
+    $(this).data('event', {
+      title: $.trim($(this).text()), // use the element's text as the event title
+      stick: true // maintain when user navigates (see docs on the renderEvent method)
     });
 
-    new_marker.addListener('click', function() {
-      infowindow.open(map, new_marker);
+    // make the event draggable using jQuery UI
+    $(this).draggable({
+      zIndex: 999,
+      revert: true,      // will cause the event to go back to its
+      revertDuration: 0  //  original position after the drag
     });
 
   });
