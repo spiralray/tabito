1bad1090c74c659da84603d8f186c56a671a8dee
spiralray <spiralray@gmail.com>
2016-05-23 10:53:37 +0900

    canvasテスト

--- assets/js/map.js
+++ assets/js/map.js
@@ -12,6 +12,76 @@ var location_marker = null;
 
 var directionsRenderers = [];
 
+window.addEventListener("load", function () {
+
+  // 必要な変数を宣言しておく
+  var canvas = document.getElementById("myCanvas");
+  var c = canvas.getContext("2d");
+  var w = 750;
+  var h = 600;
+  var drawing = false;
+  var oldPos;
+
+  // CanvasとContextを初期化する
+  canvas.width = w;
+  canvas.height = h;
+  c.strokeStyle = "#000000";
+  c.lineWidth = 5;
+  c.lineJoin = "round";
+  c.lineCap = "round";
+
+  // タップ開始時に、絵を描く準備をする
+  canvas.addEventListener("touchstart", function (event) {
+    console.log("touchstart");
+    drawing = true;
+    oldPos = getPosT(event);
+  }, false);
+
+  // タップ終了時に、絵を描く後処理を行う
+  canvas.addEventListener("touchend", function () {
+    console.log("touchend");
+    drawing = false;
+  }, false);
+
+  // gestureイベント（２本指以上で触ると発生するやつ）の
+  // 終了時にも絵を描く後処理を行う
+  canvas.addEventListener("gestureend", function () {
+    console.log("mouseout");
+    drawing = false;
+  }, false);
+
+  // 実際に絵を描く処理
+  // 前回に保存した位置から現在の位置迄線を引く
+  canvas.addEventListener("touchmove", function (event) {
+    console.log("touchmove");
+    var pos = getPosT(event);
+    if (drawing) {
+      c.beginPath();
+      c.moveTo(oldPos.x, oldPos.y);
+      c.lineTo(pos.x, pos.y);
+      c.stroke();
+      c.closePath();
+      oldPos = pos;
+    }
+  }, false);
+
+  // タップ位置を取得する為の関数群
+  function scrollX(){return document.documentElement.scrollLeft || document.body.scrollLeft;}
+  function scrollY(){return document.documentElement.scrollTop || document.body.scrollTop;}
+  function getPosT (event) {
+    var mouseX = event.touches[0].clientX - $(canvas).position().left + scrollX();
+    var mouseY = event.touches[0].clientY - $(canvas).position().top + scrollY();
+    return {x:mouseX, y:mouseY};
+  }
+
+  // 削除ボタンの動作
+  $("delete_button").click(function () {
+    c.clearRect(0, 0, $(canvas).width(), $(canvas).height());
+  });
+
+}, false);
+
+
 function initialize() {
 
   $.ajax({
